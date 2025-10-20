e1461a0e1b7572710e6a69af5cf001684b3890b8
Your name <you@example.com>
2025-10-20 11:43:29 -0600

    Added placeholder for text webhook

--- startup-react/src/input/input.jsx
+++ startup-react/src/input/input.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import './input.css';
 
 export function Input() {
+  const [prompt, setPrompt] = React.useState('Loading...');
+  React.useEffect(() => {
+    setPrompt(`Placeholder for writing prompt web service`);
+  }, []);
+
   return (
 
     <main>
         <h2>Add to the story here!</h2>
         <input type="text" id="storyInput" name="storyInput"></input>
-        <input type="submit" value="Submit"></input>
+        <input type="submit" value="Submit" onclick="submitText"></input>
 
-        <p>Placeholder for writing prompt web service</p>
+        <p>{prompt}</p>
     </main>
 
 
