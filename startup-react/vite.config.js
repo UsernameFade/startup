74f0a60b868514fbec41f9dea59334641fb84b16
Your name <you@example.com>
2025-12-03 21:34:10 -0700

    Added peeer proxy and framework for storyNotifier

--- startup-react/vite.config.js
+++ startup-react/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 
+import { defineConfig } from 'vite';
+
 export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:4000',
+      '/ws': {
+        target: 'ws://localhost:4000',
+        ws: true,
+      },
     },
   },
 });
-
